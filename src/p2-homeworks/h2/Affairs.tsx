406115a469a0b1a5339453823f2a2de4820f68b2
SnezhanaKorsak <snezhana_korsak@mail.ru>
2021-10-05 18:49:21 +0300

    HW2

---
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Affair from './Affair'
+import {AffairType, FilterType} from './HW2'
+import s from './Affairs.module.css'
+
+type AffairsPropsType = { // need to fix any
+    data: Array<AffairType>
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (id: number) => void
+    filter: FilterType
+}
+
+function Affairs(props: AffairsPropsType) {
+    const mappedAffairs = props.data.map((a: AffairType) => (
+        <Affair // should work
+            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            affair={a}
+            deleteAffairCallback={props.deleteAffairCallback}
+        />
+    ))
+
+    const setAll = () => {props.setFilter('all')
+    }
+    const setHigh = () => {props.setFilter('high')
+    }
+    const setMiddle = () => {props.setFilter('middle')
+    }
+    const setLow = () => {props.setFilter('low')}
+
+    const setClass = (filter: FilterType) => {
+        return s.button + (props.filter === filter ? ' ' + s.active : '' )
+    }
+
+    return (
+        <div>
+
+            {mappedAffairs}
+
+            <button className={setClass('all')} onClick={setAll}>All</button>
+            <button className={setClass('high')} onClick={setHigh}>High</button>
+            <button className={setClass('middle')} onClick={setMiddle}>Middle</button>
+            <button className={setClass('low')} onClick={setLow}>Low</button>
+        </div>
+    )
+}
+
+export default Affairs
